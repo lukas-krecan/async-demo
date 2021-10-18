1575ab2f10498d1a5050288f4df645578288c48d
Lukáš Křečan <lukas.krecan@productboard.com>
2021-10-18 20:57:44 +0200

    Loom sum

---
+++ thread-based/src/main/kotlin/net/javacrumbs/threadbased/DemoSumLoomController.kt
@@ -0,0 +1,45 @@
+package net.javacrumbs.threadbased
+
+import mu.KLogging
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
+
+@RestController
+class DemoSumLoomController {
+
+    @GetMapping("/demoSumLoom")
+    fun demo(
+        @RequestParam(defaultValue = "3") n: Int,
+        @RequestParam(defaultValue = "100") delay: Long,
+        @RequestParam(defaultValue = "false") log: Boolean
+    ): Result {
+        if (log) logger.info { "Will generate sum of random numbers" }
+        try {
+            return Executors.newVirtualThreadExecutor().use { executor ->
+                val result = (0..n).map {
+                    executor.submit(Callable { getRandomNumber(delay) })
+                }.sumOf {
+                    // Blocking on get
+                    it.get().number
+                }
+                Result(result)
+            }
+        } catch (e: Exception) {
+            logger.error(e) { "Error when generating random number" }
+            throw e
+        } finally {
+            if (log) logger.info { "Finished" }
+        }
+    }
+
+    private fun getRandomNumber(delay: Long): RandomNumber {
+        return webClient.get().uri("/random?delay={delay}", delay).retrieve().bodyToMono(RandomNumber::class.java)
+            .block()!!
+    }
+
+    companion object : KLogging()
+}
