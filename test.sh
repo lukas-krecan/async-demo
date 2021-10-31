17a81ee301389f196aa213f12b56bfdf7d9385cc
Lukáš Křečan <lukas.krecan@productboard.com>
2021-10-31 20:06:24 +0100

    Better test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Warmup for 2 seconds"
-echo "GET http://localhost:8081/$1" | vegeta -cpus=2 attack -duration=3s -rate=10 > /dev/null
+echo "GET http://localhost:8081/$1" | vegeta -cpus=2 attack -duration=3s -rate=1000 > /dev/null
 echo "Test for 10 seconds"
-echo "GET http://localhost:8081/$1" | vegeta -cpus=2 attack -duration=10s -rate=$2 > result.bin; vegeta plot result.* > out.html; open out.html
+echo "GET http://localhost:8081/$1" | vegeta -cpus=2 attack -duration=7s -rate=$2 > result.bin; vegeta plot result.* > out.html; open out.html
 vegeta report result.bin
