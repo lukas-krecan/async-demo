4bf387a4c2d10559a4d30f5e70a055e9d0dc278a
Lukáš Křečan <lukas.krecan@productboard.com>
2021-11-01 14:44:47 +0100

    Error log

--- thread-based/src/main/kotlin/net/javacrumbs/threadbased/DemoController.kt
+++ thread-based/src/main/kotlin/net/javacrumbs/threadbased/DemoController.kt
@@ -4,7 +4,6 @@ import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.client.WebClient
 
 
 @RestController
